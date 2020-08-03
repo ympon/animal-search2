cdd39ea868c42d4b7b8a99349f0956d5cb7f4fcc
ympon <tyasson0u0@gmail.com>
2020-08-03 21:50:20 +0900

    edit-posts-controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @posts = Post.all
   end
@@ -17,5 +19,11 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:image, :text)
+  end
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
